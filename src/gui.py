e39600af2aa25c57ffc401b42a7369ed8433a877
Sebastian Schmitt <schmitts@rhrk.uni-kl.de>
2023-04-07 21:26:43 +0200

    Update src/gui.py

--- src/gui.py
+++ src/gui.py
@@ -2,7 +2,8 @@
 
 class GUI():
     def __init__(self):
-        layout = [[sg.Text('LTD Kicker App')],
+        layout = [[sg.Text('LTD Kicker App', font='Arial 24')],
+                  [[PLAY_BUTTON(nr) for nr in [1,2]],---Ergebnis eingeben---,[PLAY_BUTTON(nr) for nr in [3,4]],
                   ]
 
         self.mainWindow = sg.Window('LTD Kicker App', layout,
@@ -11,12 +12,20 @@ class GUI():
 
     def run(self):
         while True:
+            event, values = self.mainWindow.read()
+
+            if event == sg.WINDOW_CLOSED:
+                break
+            elif event in ['but_player_1' + nr for nr in range(1,5)]:
+                # Open window to select player
 
 # Auxiliary classes and function
 # Class to define player buttons
 class PLAY_BUTTON():
-    def __init__(self,text):
-        sg.Button(  button_text = text,
-                    s = (10,10))
+    def __init__(self, id):
+        sg.Button(  button_text = 'Spieler auswählen',
+                    key = 'but_player_' + id,
+                    s = (10,10),
+                    font = 'Arial 12')
             
             
